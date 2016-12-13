2e0c821c811da7901b6f79b80f20da7d95be8a14
choudhs2 <choudhs2@bxscience.edu>
2016-12-13 13:21:05 -0500

    Add files via upload

---
+++ Minesweeper.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Minesweeper {
+  public static void main(String[] args) {
+    System.out.println("How many rows/columns?");
+    String[][][] layers = new String[2][n][n];
+  }
+}
