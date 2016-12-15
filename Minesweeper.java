963de93c396156fbd97bbc18334c718112281606
choudhs2 <choudhs2@bxscience.edu>
2016-12-15 09:49:48 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -3,7 +3,9 @@ public class Minesweeper {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);//REMEMBER THIS IS THE INITIALIZED SCANNER!!!
     System.out.println("How many rows/columns?");
-    int n = s.nextInt();
-    String[][][] layers = new String[2][n][n];//this is the extra cred part
+    int inp = s.nextInt();//for all inputs, non-menu
+    String[][][] layers = new String[2][inp][inp];//this is the extra cred part
+    System.out.println("Select the number next to the difficulty you want\n1. Easy\n2. Medium\n3. Hard\nOther inputs will 'Quit'");
+    int menuInp = s.nextInt();//if 1, put 15% mines, if 2, 20%, if 3, 25%, else quit loop
   }
 }
