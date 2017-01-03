db1c500bc6c4a3e409bc59009bb186feb870b675
choudhs2 <choudhs2@bxscience.edu>
2017-01-02 19:09:46 -0500

    zeroes still dont wrokkkkkkkkk oh well

--- Minesweeper.java
+++ Minesweeper.java
@@ -6,7 +6,7 @@ public class Minesweeper {
     //Scanner s = new Scanner(System.in);//REMEMBER THIS IS THE INITIALIZED SCANNER!!!
     System.out.println("How many rows/columns? (enter one number, it is a square grid)");
     int inp = s.nextInt();//for all inputs, non-menu
-    String[][][] layers = new String[2][inp][inp];//3D would be the extra cred part
+    String[][][] layers = new String[2][inp + 2][inp + 2];//3D would be the extra cred part
     //Possible gui in the future
     System.out.println("Select the number next to the difficulty you want\n1. Easy\n2. Medium\n3. Hard\nOther inputs will 'Quit'");
     int m = s.nextInt();//if 1, put 15% mines, if 2 => 20%, if 3 => 25%, else quit
@@ -61,9 +61,9 @@ public class Minesweeper {
   }
   
   public static boolean completionCheck(String[][][] l) {
-    for (int i = 0; i <l[0].length; i++) {
-      for (int j = 0; j <l[0][0].length; j++) {
-        if (!(l[0][i][j].equals("F") || l[0][i][j].equals("SHOW"))) {
+    for (int i = 1; i <l[0].length-1; i++) {
+      for (int j = 1; j <l[0][0].length-1; j++) {
+        if (!((l[1][i][j].equals("X") && l[0][i][j].equals("F")) || l[0][i][j].equals("SHOW"))) {
           return false;
         }
       }
@@ -74,94 +74,22 @@ public class Minesweeper {
   public static String[][][] showZeroes(String[][][] l, int[] c) {//does not work yet     DOES NOT WORK YET
     l[0][c[0]][c[1]] = "SHOW";
     if (c[0] > 0 && c[0] < l[1].length-1 && c[1] > 0 && c[1] < l[1][0].length-1) {
-      for (int i = c[0]-1; i < c[0]+2; i++) {
+      /*for (int i = c[0]-1; i < c[0]+2; i++) {
         for (int j = c[1]-1; j < c[1]+2; j++) {
           int[] a = {i, j};
           if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
             l = showZeroes(l, a);
           }
         }
-      }
-    }
-    else if (c[0] == 0 && c[1] > 0 && c[1] < l[0][0].length-1) {
-      for (int i = c[0]; i < c[0]+2; i++) {
-        for (int j = c[1]-1; j < c[1]+2; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[1] == 0 && c[0] > 0 && c[0] < l[0].length-1) {
-      for (int i = c[0]-1; i < c[0]+2; i++) {
-        for (int j = c[1]; j < c[1]+2; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[0] == l[0].length-1 && c[1] > 0 && c[1] < l[0][0].length-1) {
-      for (int i = c[0]-1; i < c[0]+1; i++) {
-        for (int j = c[1]-1; j < c[1]+2; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[1] == l[0][0].length-1 && c[0] > 0 && c[0] < l[0].length-1) {
-      for (int i = c[0]-1; i < c[0]+2; i++) {
-        for (int j = c[1]-1; j < c[1]+1; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[0] == 0 && c[1] == 0) {
-      for (int i = c[0]; i < c[0]+2; i++) {
-        for (int j = c[1]; j < c[1]+2; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[0] == l[0].length-1 && c[1] == 0) {
-      for (int i = c[0]-1; i < c[0]+1; i++) {
-        for (int j = c[1]; j < c[1]+2; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[0] == 0 && c[1] == l[0][0].length-1) {
-      for (int i = c[0]; i < c[0]+2; i++) {
-        for (int j = c[1]-1; j < c[1]+1; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
-    }
-    else if (c[0] == l[0].length-1 && c[1] == l[0][0].length-1) {
-      for (int i = c[0]-1; i < c[0]+1; i++) {
-        for (int j = c[1]-1; j < c[1]+1; j++) {
-          int[] a = {i, j};
-          if (l[1][i][j].equals("0") && i!=c[0] && j!=c[1]) {
-            l = showZeroes(l, a);
-          }
-        }
-      }
+      }*/
+      int[] a = {c[0]-1, c[1]-1};
+      l=showZeroes(l, a);
+      int[] b = {c[0]-1, c[1]+1};
+      l=showZeroes(l, b);
+      int[] g = {c[0]+1, c[1]-1};
+      l=showZeroes(l, g);
+      int[] d = {c[0]+1, c[1]+1};
+      l=showZeroes(l, d);
     }
   return l;
   }
@@ -182,93 +110,20 @@ public class Minesweeper {
         }
       }
     }
-    for (int z = 1; z < l[1][0].length-1; z++) {//top
-      if (!(l[1][0][z].equals("X"))) {
-        int count = 0;
-        for (int j = 0; j < 2; j++) {
-          for (int k = z-1; k <z+2; k++) {
-            if (!(k==z && j==0) && (l[1][j][k].equals("X"))) {
-              count += 1;
-            }
-          }
-        }
-        l[1][0][z] = "" + count;
-      }
-    }
-    for (int z = 1; z < l[1][0].length-1; z++) {//bottom
-      if (!(l[1][(l[1][0].length-1)][z].equals("X"))) {
-        int count = 0;
-        for (int j = l[1][0].length-2; j < l[1][0].length; j++) {
-          for (int k = z-1; k <z+2; k++) {
-            if (!(k==z && j==l[1][0].length-1) && (l[1][j][k].equals("X"))) {
-              count += 1;
-            }
-          }
-        }
-        l[1][l[1][0].length-1][z] = "" + count;
-      }
-    }
-    for (int z = 1; z < l[1].length-1; z++) {//left
-      if (!(l[1][z][0].equals("X"))) {
-        int count = 0;
-        for (int j = 0; j < 2; j++) {
-          for (int k = z-1; k <z+2; k++) {
-            if (!(k==0 && j==z) && (l[1][k][j].equals("X"))) {
-              count += 1;
-            }
-          }
-        }
-        l[1][z][0] = "" + count;
-      }
-    }
-    for (int z = 1; z < l[1].length-1; z++) {//left
-      if (!(l[1][z][l[1].length-1].equals("X"))) {
-        int count = 0;
-        for (int j = l[1].length-2; j < l[1].length; j++) {
-          for (int k = z-1; k <z+2; k++) {
-            if (!(k==l[1].length-1 && j==z) && (l[1][k][j].equals("X"))) {
-              count += 1;
-            }
-          }
-        }
-        l[1][z][l[1].length-1] = "" + count;
-      }
-    }
-    //corners now MAKE THIS METHOD MUCH MORE EFFICIENT i hate my life its 3 am
-    if (!(l[1][0][0].equals("X"))) {
-      int count = 0;
-      if (l[1][0][1].equals("X")) {count+=1;} if (l[1][1][0].equals("X")) {count+=1;} if (l[1][1][1].equals("X")) {count +=1;}
-      l[1][0][0] = "" +count;
-    }
-    if (!(l[1][0][l[1][0].length-1].equals("X"))) {
-      int count = 0;
-      if (l[1][0][l[1][0].length-2].equals("X")) {count+=1;} if (l[1][1][l[1][0].length-1].equals("X")) {count+=1;} if (l[1][1][l[1][0].length-2].equals("X")) {count +=1;}
-      l[1][0][l[1][0].length-1] = "" +count;
-    }
-    if (!(l[1][l[1].length-1][0].equals("X"))) {
-      int count = 0;
-      if (l[1][l[1].length-1][1].equals("X")) {count+=1;} if (l[1][l[1].length-2][0].equals("X")) {count+=1;} if (l[1][l[1].length-2][1].equals("X")) {count +=1;}
-      l[1][l[1].length-1][0] = "" +count;
-    }
-    if (!(l[1][l[1].length-1][l[1][0].length-1].equals("X"))) {
-      int count = 0;
-      if (l[1][l[1].length-1][l[1][0].length-2].equals("X")) {count+=1;} if (l[1][l[1].length-2][l[1][0].length-1].equals("X")) {count+=1;} if (l[1][l[1].length-2][l[1][0].length-2].equals("X")) {count +=1;}
-      l[1][l[1].length-1][l[1][0].length-1] = "" +count;
-    }
-    return l;//i hate everything
+    return l;
   }
   
   public static int[] inpCoords(int inp) {
-    System.out.println("Enter row number (0 to number of rows/columns you chose-1) then the column (same rule)");
+    System.out.println("Enter row number (1 to number of rows/columns you chose) then the column in the second box (same rule)");
     int[] n = {s.nextInt(), s.nextInt()};
-    if (n[0] < 0 || n[0] > inp-1 || n[1] < 0 || n[1] > inp-1) {
+    if (n[0] < 1 || n[0] > inp || n[1] < 1 || n[1] > inp) {
       n = inpCoords(inp);
     }
     return n;
   }
   
   public static String[][][] setUpMinesweeperMines(int rowCol, int dif) {
-    String[][][] res = new String[2][rowCol][rowCol];//final result
+    String[][][] res = new String[2][rowCol+2][rowCol+2];//final result
     int numMInes = (int) (Math.pow(rowCol, 2) * (.15 + ((((double) dif - 1)*5)/100))); // makes number of mines from percent in
     // one step, complicated, but it should work
     for (int i = 0; i < res[0].length; i++) {
@@ -281,8 +136,8 @@ public class Minesweeper {
     int row = 0;
     int col = 0;
     while (placedMines != numMInes) {//places mines randomly
-      row = (int) Math.floor(Math.random()*rowCol);
-      col= (int) Math.floor(Math.random()*rowCol);
+      row = (int) Math.floor(Math.random()*rowCol) + 1;
+      col= (int) Math.floor(Math.random()*rowCol) + 1;
       if (res[1][row][col].equals(" ")) {
         res[1][row][col] = "X";//mines placed
         placedMines+=1;
@@ -293,9 +148,9 @@ public class Minesweeper {
   
   public static void printMinefield(String[][][] l) {
     String s = "";
-    for (int i = 0; i<l[0].length; i++) {
+    for (int i = 1; i<l[0].length-1; i++) {
       s ="|";
-      for (int j = 0; j<l[0][0].length; j++) {
+      for (int j = 1; j<l[0][0].length-1; j++) {
         if (l[0][i][j].equals("SHOW")) {//if value is show, it shows the bottom
           s+=l[1][i][j] + "|";
         }
@@ -308,8 +163,8 @@ public class Minesweeper {
   }
   
   public static void loss(String[][][] l) {
-    for (int i = 0; i < l[0].length; i++) {
-      for (int j = 0; j<l[0][i].length; j++) {
+    for (int i = 1; i < l[0].length-1; i++) {
+      for (int j = 1; j<l[0][i].length-1; j++) {
         l[0][i][j] = "SHOW";
       }
     }
